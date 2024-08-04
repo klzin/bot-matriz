798e1aadc51ed2890391752ac1b96b8ab5561450
klzin <125775752+klzin@users.noreply.github.com>
2024-08-04 15:30:09 -0300

    Update config.py
    REMOVENDO TOKEN DE SEGURANÇA

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ init(autoreset=True)
 
 # Constantes
 DB_NAME = 'arquivos/matrizes.db'
-TOKEN = '7054218468:AAHe_tF3xpQSrPxu3G5kD1mu28uZ06ITnSE'
+TOKEN = ''
 MESSAGE_LIMIT = 4096
 GROUP_ID = -4246690879  # ID do grupo para onde enviar as mensagens de log
